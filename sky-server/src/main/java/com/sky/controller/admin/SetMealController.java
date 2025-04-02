2a3a5a6799274939b5de2f64559dbcf0d1415d3a
0123mingming <1834792335@qq.com>
2025-04-02 19:36:05 +0800

    套餐删除代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -1,16 +1,17 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetMealService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 套餐管理
@@ -24,15 +25,60 @@ public class SetMealController {
     private SetMealService setMealService;
 
     /**
-     *  新增套餐
+     * 新增套餐
+     *
      * @param setmealDTO
      * @return
      */
     @PostMapping
     @ApiOperation("新增套餐")
-    public Result save(@RequestBody SetmealDTO setmealDTO){
-         log.info("新增套餐：{}",setmealDTO.toString());
-         setMealService.saveWithDish(setmealDTO);
-         return Result.success();
+    public Result save(@RequestBody SetmealDTO setmealDTO) {
+        log.info("新增套餐：{}", setmealDTO.toString());
+        setMealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 处理分页查询请求
+     *
+     * 该方法通过接收一个 SetmealPageQueryDTO 对象作为参数，用于指定分页查询的条件和参数
+     * 使用 @GetMapping 注解指定 HTTP GET 请求的处理，@ApiOperation 注解用于 Swagger 文档生成，描述该方法的作用
+     *
+     * @param setmealPageQueryDTO 分页查询的条件和参数对象，包含页码、每页数量等信息
+     * @return 返回一个 Result 对象，其中包含分页查询的结果 PageResult
+     *         Result.success(pageResult) 表示查询成功，返回包含分页数据的 PageResult 对象
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
+        // 记录分页查询的日志信息
+        log.info("分页查询：{}", setmealPageQueryDTO);
+        // 调用 service 层的分页查询方法，并返回查询结果
+        PageResult pageResult = setMealService.pageQuery(setmealPageQueryDTO);
+        // 返回包含分页查询结果的响应
+        return Result.success(pageResult);
+
+    }
+
+    /**
+     * 批量删除套餐接口
+     *
+     * 该接口接收一个套餐ID列表，将这些套餐从系统中删除
+     * 主要用于后台管理，当需要清理多个套餐时，可以使用该接口
+     *
+     * @param ids 待删除的套餐ID列表，每个ID代表一个套餐
+     * @return 返回删除结果，如果删除成功，返回成功结果
+     */
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    public Result delete(@RequestParam List<Long> ids){
+        // 记录日志，追踪删除操作
+        log.info("批量删除套餐：{}", ids);
+
+        // 调用服务层方法，执行批量删除操作
+        setMealService.deleteBatch(ids);
+
+        // 返回成功结果，表示删除操作完成
+        return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
@@ -1,12 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetMealDishMapper;
 import com.sky.mapper.SetMealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetMealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +49,51 @@ public class SetMealServiceImpl implements SetMealService {
         //保存套餐和菜品的关联关系
         setMealDishMapper.insertBatch(setmealDishes);
 
+    }
+
+    /**
+     * 根据条件分页查询套餐信息
+     *
+     * @param setmealPageQueryDTO 包含分页信息和查询条件的对象
+     * @return 返回分页查询结果，包括总记录数和套餐数据列表
+     */
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        // 提取页码和页面大小信息
+        int pageNum=setmealPageQueryDTO.getPage();
+        int pageSize=setmealPageQueryDTO.getPageSize();
+
+        // 启用MyBatis分页插件，设置当前页和页面大小
+        PageHelper.startPage(pageNum,pageSize);
+        // 执行分页查询，获取包含总记录数和数据列表的Page对象
+        Page<SetmealVO> page=setmealMapper.pageQuery(setmealPageQueryDTO);
+        // 根据查询结果构建并返回PageResult对象
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+    /**
+     * 根据多个ID删除套餐信息
+     * 此方法首先检查每个套餐的状态，确保它们未启用如果套餐已启用，则抛出异常不允许删除
+     * 对于未启用的套餐，方法将它们从数据库中删除，并同时删除与这些套餐关联的菜品信息
+     *
+     * @param ids 要删除的套餐的ID列表
+     * @throws DeletionNotAllowedException 如果尝试删除启用的套餐，则抛出此异常
+     */
+    @Override
+    public void deleteBatch(List<Long> ids) {
+        // 检查每个套餐的状态，确保它们未启用
+        ids.forEach(id->{
+            Setmeal setmeal=setmealMapper.getById(id);
+            // 如果套餐已启用，抛出异常不允许删除
+            if(StatusConstant.ENABLE==setmeal.getStatus()){
+                throw new DeletionNotAllowedException(MessageConstant.SETMEAL_ON_SALE);
+            }
+        });
+
+        // 删除未启用的套餐，并同时删除与这些套餐关联的菜品信息
+        ids.forEach(id->{
+            setmealMapper.deleteById(id);
+            setMealDishMapper.deleteBySetmealId(id);
+        });
     }
 }
